d1231a76d35a603319ecfc786df359da82fb5ea6
Ponlork-PK <rousponlork@gmail.com>
2025-05-12 22:05:52 +0700

    12/05/2025

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -12,45 +12,132 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.bold,
-            color: Colors.deepPurple,
+            color: Colors.blue,
           ),
         ),
+        elevation: 8,
+        actions: [
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9jZ2FzVxFkw-yBn7FM0dOJRzLD26gS5Ro1w&s'),
+            ),
+          )
+        ],
       ),
-      body: Container(
-        height: 200,
-        color: Colors.amber,
-        child: Column(
+      drawer: Drawer(
+        width: 250,
+        child: ListView(
           children: [
-            Text(
-              "Hello",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurpleAccent,
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Setting',
+                style: TextStyle(color: Colors.blueAccent),
               ),
+              onTap: (){},
             ),
-            Text(
-              "Welcome to Cambodia!",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.orange,
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Setting',
+                style: TextStyle(color: Colors.blueAccent),
               ),
+              onTap: (){},
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.location_city,
-              )
-            ),
-            Text(
-              "Love you",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.blue,
+            ListTile(
+              leading: Icon(Icons.contact_page),
+              title: Text(
+                'About Us',
+                style: TextStyle(color: Colors.blueAccent),
               ),
+              onTap: (){},
             ),
           ],
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Amount: ',
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: SizedBox(
+                  width: 300,
+                  height: 100,
+                  child:
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: (){}, 
+                      child: Text(
+                        'Spending List',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      )
+                    ),
+                  
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: SizedBox(
+                  width: 300,
+                  height: 100,
+                  child:
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: (){}, 
+                      child: Text(
+                        'History',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      )
+                    ),
+                  
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
